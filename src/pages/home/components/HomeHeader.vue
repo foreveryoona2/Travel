a38a12bb51c668363dcca44c2a8b4afcc1566884
foreveryoona <foreveryoona2@163.com>
2018-07-15 00:29:09 +0800

    'city-router'

--- src/pages/home/components/HomeHeader.vue
+++ src/pages/home/components/HomeHeader.vue
@@ -7,11 +7,13 @@
 			<span class="iconfont">&#xe632;</span>
 			输入城市/景点/国民主题
 		</div>
-		<div class="header-right">
-			{{this.city}}
-			<span class="iconfont arrow-icon">&#xe64a;</span>
-			
-		</div>
+		<router-link to="/city">
+			<div class="header-right">
+				{{this.city}}
+				<span class="iconfont arrow-icon">&#xe64a;</span>
+			</div>			
+		</router-link>
+
 	</div>
 	
 </template>
@@ -30,7 +32,7 @@ export default {
 @import '~styles/varibles.styl'
 .header
 	display:flex
-	line-height .86rem
+	line-height $headerHeight
 	background $bgColor
 	color:#fff
 	.header-left
@@ -53,6 +55,7 @@ export default {
 		width 1.24rem
 		float:right
 		text-align:center
+		color #fff
 		.arrow-icon
 			margin-left:.1
 			text-align:center
