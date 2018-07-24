63a9889a2a20423b028897daec68fd512b2ad27e
foreveryoona <foreveryoona2@163.com>
2018-07-24 23:29:58 +0800

    'bannerFinish'

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,69 @@
+<template>
+	<div class="container" @click="handleContainerClick">
+		<div class="wrapper">
+			<swiper :options="swiperOptions">
+				<swiper-slide v-for="(item,index) of imgs" :key="index">
+					<img class="gallary-img" :src="item" alt="">
+				</swiper-slide>
+				<div class="swiper-pagination"  slot="pagination"></div>
+			</swiper>	
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'CommonGallary',
+	props: {
+		imgs: {
+			type: Array,
+			default () {
+				return []
+			}
+		}
+	},
+	data () {
+		return {
+			showGallary: false,
+			swiperOptions: {
+				pagination: '.swiper-pagination',
+				paginationType: 'fraction',
+				observeParents: true,
+				observer: true
+				//检测到dom变化即会触发刷新一次
+			}
+		}
+	},
+	methods: {
+		handleContainerClick () {
+			this.$emit('close')
+		}
+	}
+};	
+</script>
+
+<style lang="stylus" scoped>
+.container >>> .swiper-container
+	overflow inherit
+.container
+	display flex
+	flex-direction column
+	justify-content center
+	position fixed
+	left 0
+	right 0
+	top 0
+	bottom 0
+	background-color #000
+	z-index 99
+	.wrapper
+		width 100%
+		height 0
+		padding-bottom 58.7%
+		.gallary-img
+			width 100%
+		.swiper-pagination
+			color #fff
+			bottom -1rem
+			z-index 1000
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,80 @@
+<template>
+	<div>
+		<div class="banner" @click="handleShowGallary">
+			<img class="banner-img" src="https://imgs.qunarzz.com/p/tts2/1804/97/382b3bd308aea002.jpg_750x440_43baa2df.jpg" >
+			<div class="banner-info">
+				<div class="banner-title">
+					出发地:北京 北京旭冉之旅
+				</div>
+				<div class="banner-number">
+					<span class="iconfont banner-icon">&#xe663;</span>
+					39
+				</div>
+			</div>
+		</div>	
+		<common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>	
+	</div>
+
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+	name: 'DetailBanner',
+	data () {
+		return {
+			showGallary: false,
+			imgs: ['https://imgs.qunarzz.com/p/tts2/1804/97/382b3bd308aea002.jpg_750x440_43baa2df.jpg','https://imgs.qunarzz.com/p/tts4/1807/d1/87f9594327e9f602.jpg_750x440_2c325cee.jpg']
+		}
+
+		
+	},
+	methods: {
+		handleShowGallary () {
+			this.showGallary = true
+		},
+		handleGallaryClose () {
+			this.showGallary = false
+		}
+	},
+	components: {
+		CommonGallary
+	}
+};
+</script>
+<style lang="stylus" scoped>
+.banner
+	position relative
+	overflow hidden
+	height 0
+	padding-bottom 58.7%
+	color #fff
+	// height 0 + padding-bottom 58.7%(图片高度/图片宽度)得出的百分比数值，这种写法可避免初次加载页面时，此布局部分出现真空抖动
+	.banner-img
+		width 100%
+	.banner-info
+		display flex
+		position absolute
+		left 0
+		right 0
+		bottom 0
+		line-height .6rem
+		background-image linear-gradient(top,rgba(0,0,0,0,), rgba(0,0,0,0.4))
+		.banner-title
+			flex 1
+			font-size .32rem
+			padding 0 .2rem
+		.banner-number
+			margin-top .14rem
+			padding 0 .4rem
+			line-height .32rem
+			height .32rem
+			border-radius .2rem
+			background-color rgba(0,0,0,0.2)
+			font-size .24rem
+			.banner-icon
+				font-size .24rem
+				
+			
+		
+</style>
